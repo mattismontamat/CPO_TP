33e959ca429b9814dcad0a0100269577621e38f2
matti <mattis.montamat@epfedu.fr>
2022-10-21 17:03:32 +0200

    avancé du TP2 jusqu'a exo 12

---
+++ TP2_convertisseurObjet_MONTAMAT/src/tp2_convertisseurobjet_montamat/Convertisseur.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2_convertisseurobjet_montamat;
+
+/**
+ *
+ * @author matti
+ */
+public class Convertisseur {
+    public Converstisseur (int nb ){
+    nbConvertisseur= nb
+            }
+    
+    public void Convertisseur();{
+}
+    public double CversK (double a ){
+        
+}
+    public double KVersC (double b){
+        
+    }
+    public double FVersC (double c){
+     
+    }
+    public double CVersF (double d){
+        
+    }
+    public double FVersK (double e){
+        
+    }
+    public double KVersF (double f){
+        
+    }
+    public double toString (){
+        
+    }
+    }
+
+    
+    
