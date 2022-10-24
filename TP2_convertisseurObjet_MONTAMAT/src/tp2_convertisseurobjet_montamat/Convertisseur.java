16c8ad5c600f610310782c8a02f1edae7ae22338
matti <mattis.montamat@epfedu.fr>
2022-10-24 13:16:10 +0200

    exo1: question 13 exo2: question3

--- TP2_convertisseurObjet_MONTAMAT/src/tp2_convertisseurobjet_montamat/Convertisseur.java
+++ TP2_convertisseurObjet_MONTAMAT/src/tp2_convertisseurobjet_montamat/Convertisseur.java
@@ -9,33 +9,45 @@ package tp2_convertisseurobjet_montamat;
  * @author matti
  */
 public class Convertisseur {
-    public Converstisseur (int nb ){
-    nbConvertisseur= nb
+    
+    int nbConversions;
+    public Convertisseur (){
+    nbConversions= 0;
             }
     
-    public void Convertisseur();{
+    public void Convertisseur(){
 }
     public double CversK (double a ){
-        
+        a=a+273 ;
+ 
+        return a;
 }
     public double KVersC (double b){
-        
+        b=b-273;
+        return b;
     }
     public double FVersC (double c){
-     
+        c=(c*1.8)+32;
+     return c;
     }
     public double CVersF (double d){
+        d=(d-32)/1.8;
+        return d;
         
     }
     public double FVersK (double e){
-        
+       e = (e + 459.67) * 5/9 ;
+       return e;
     }
     public double KVersF (double f){
-        
+      f=(f *9/5) - 459.67;
+      return f; 
     }
     public double toString (){
         
     }
+
+}
     }
 
     
