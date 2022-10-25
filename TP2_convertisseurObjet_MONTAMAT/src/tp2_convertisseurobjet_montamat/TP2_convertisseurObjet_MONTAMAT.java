bce24fc120e23d9459cbca272217082101430c65
matti <mattis.montamat@epfedu.fr>
2022-10-25 09:50:06 +0200

    exo1: question 13 exo2: question3 exo3: question 5 exo4: pas commencé

--- TP2_convertisseurObjet_MONTAMAT/src/tp2_convertisseurobjet_montamat/TP2_convertisseurObjet_MONTAMAT.java
+++ TP2_convertisseurObjet_MONTAMAT/src/tp2_convertisseurobjet_montamat/TP2_convertisseurObjet_MONTAMAT.java
@@ -4,6 +4,8 @@
  */
 package tp2_convertisseurobjet_montamat;
 
+import java.util.Scanner;
+
 /**
  *
  * @author matti
@@ -14,7 +16,25 @@ public class TP2_convertisseurObjet_MONTAMAT {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
- Convertisseur.CVersK (34);
-  
+ 
+    double saisi_n;
+    saisi_n=0;
+    double saisi_c;
+    saisi_c=0;
+    double value;
+    value=0;
+    
+    Scanner sc;
+    sc=new Scanner(System.in);
+    
+    System.out.println("saisissez la valeur de votre température");
+    saisi_n=sc.nextDouble();
+    System.out.println("Quelle conversion souhaitez vous faire ? \n1: Celcius vers Kelvin\n2: Celcius vers Farenheit\n3: Kelvin vers Celcius\n4: Kelvin vers Farenheit\n5: Farenheit vers Celcius\n6: Farenheit vers Kelvin");
+    saisi_c= sc.nextDouble();
+            
+  if (saisi_c==1) {
+      value= CVersK(saisi_n);
+      System.out.println("="+value+"K");
+  }
 }
 }
