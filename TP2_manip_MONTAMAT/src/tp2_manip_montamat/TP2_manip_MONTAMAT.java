f838779595b7a7259f6203feeed9afd16eae0f3d
matti <mattis.montamat@epfedu.fr>
2022-10-25 11:07:50 +0200

    exo1: fini exo2: fini exo3: question 5 -> fini exo4: pas commencé

--- TP2_manip_MONTAMAT/src/tp2_manip_montamat/TP2_manip_MONTAMAT.java
+++ TP2_manip_MONTAMAT/src/tp2_manip_montamat/TP2_manip_MONTAMAT.java
@@ -14,19 +14,19 @@ public class TP2_manip_MONTAMAT {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-Tartiflette assiette1 = new Tartiflette(500) ;
-Tartiflette assiette2 = new Tartiflette(600) ;
-Tartiflette assiette3 = assiette2 ; 
-System.out.println("nb de calories de Assiette 1 : " + assiette1.nbCalories) ;
-System.out.println("nb de calories de Assiette 2 : " +assiette2.nbCalories) ;
-System.out.println("nb de calories de Assiette 3 : " +assiette3.nbCalories) ;
+        Tartiflette assiette1 = new Tartiflette(500);
+        Tartiflette assiette2 = new Tartiflette(600);
+        Tartiflette assiette3 = assiette2;
+        System.out.println("nb de calories de Assiette 1 : " + assiette1.nbCalories);
+        System.out.println("nb de calories de Assiette 2 : " + assiette2.nbCalories);
+        System.out.println("nb de calories de Assiette 3 : " + assiette3.nbCalories);
 
-Moussaka[]tab;
-tab=new Moussaka[10];
-for(int i =1; i<=10;i++){
-    tab[i]= new Moussaka(i);
-    
-}
-    
+        Moussaka[] tab;
+        tab = new Moussaka[10];
+        for (int i = 1; i <= 10; i++) {
+            tab[i] = new Moussaka(i);
+
+        }
+
     }
 }
