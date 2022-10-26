c5cc5578ac8962f619ce3556b05c46eadef7cbd8
matti <mattis.montamat@epfedu.fr>
2022-10-26 21:17:50 +0200

    impossible de sortir de la boucle

--- TP2_convertisseurObjet_MONTAMAT/src/tp2_convertisseurobjet_montamat/TP2_convertisseurObjet_MONTAMAT.java
+++ TP2_convertisseurObjet_MONTAMAT/src/tp2_convertisseurobjet_montamat/TP2_convertisseurObjet_MONTAMAT.java
@@ -22,47 +22,56 @@ public class TP2_convertisseurObjet_MONTAMAT {
         double saisi_c;
 
         double value;
+        
+        
 
         Scanner sc;
         sc = new Scanner(System.in);
         Convertisseur c = new Convertisseur();
+        
 
-        System.out.println("saisissez la valeur de votre température");
-        saisi_n = sc.nextDouble();
-        System.out.println("Quelle conversion souhaitez vous faire ? \n1: Celcius vers Kelvin\n2: Celcius vers Farenheit\n3: Kelvin vers Celcius\n4: Kelvin vers Farenheit\n5: Farenheit vers Celcius\n6: Farenheit vers Kelvin\n7 quitter");
-        saisi_c = sc.nextDouble();
+        saisi_c = 0;
+        do {
 
-        while (saisi_c != 7) {
+            System.out.println("Quelle conversion souhaitez vous faire ? \n1: Celcius vers Kelvin\n2: Celcius vers Farenheit\n3: Kelvin vers Celcius\n4: Kelvin vers Farenheit\n5: Farenheit vers Celcius\n6: Farenheit vers Kelvin\n7 non je souhaite quitter");
+            saisi_c = sc.nextDouble();
+            System.out.println("saisissez la valeur de votre température");
+            saisi_n = sc.nextDouble();
+
             if (saisi_c == 1) {
                 value = c.CVersK(saisi_n);
                 System.out.println("=" + value + "K");
-                c.toString();
+
             }
             if (saisi_c == 2) {
                 value = c.CVersF(saisi_n);
                 System.out.println("=" + value + "F");
-                c.toString();
+
             }
             if (saisi_c == 3) {
                 value = c.KVersC(saisi_n);
                 System.out.println("=" + value + "°C");
-                c.toString();
+
             }
             if (saisi_c == 4) {
                 value = c.KVersF(saisi_n);
                 System.out.println("=" + value + "F");
-                c.toString();
+
             }
             if (saisi_c == 5) {
                 value = c.FVersC(saisi_n);
                 System.out.println("=" + value + "°C");
-                c.toString();
+
             }
             if (saisi_c == 6) {
                 value = c.FVersK(saisi_n);
                 System.out.println("=" + value + "K");
-                c.toString();
+
             }
-        }
+
+        } while (saisi_c != 7);
+        
+        System.out.println("nombre de conversions" +c);
+
     }
 }
