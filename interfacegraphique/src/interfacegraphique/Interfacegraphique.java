f6ac8a92fe64ec6077fea13549786453abbfc8b4
matti <mattis.montamat@epfedu.fr>
2022-11-07 11:12:13 +0100

    création d'un nouveau projet graphique

--- interfacegraphique/src/interfacegraphique/Interfacegraphique.java
+++ interfacegraphique/src/interfacegraphique/Interfacegraphique.java
@@ -13,8 +13,6 @@ public class Interfacegraphique {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+  
     
 }
