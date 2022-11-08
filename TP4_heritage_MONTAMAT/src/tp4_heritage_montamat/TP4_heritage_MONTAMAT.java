d0f9e7bf74b09b856bb4bb309e9d7e3abb97c115
matti <mattis.montamat@epfedu.fr>
2022-11-08 15:00:10 +0100

    création de la classe arme

---
+++ TP4_heritage_MONTAMAT/src/tp4_heritage_montamat/TP4_heritage_MONTAMAT.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp4_heritage_montamat;
+
+/**
+ *
+ * @author matti
+ */
+public class TP4_heritage_MONTAMAT {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
